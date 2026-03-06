3f4a6372722483e6336ee402b53389139ed501fd
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-06 16:09:59 -0300

    Adiciona o botão de resetar e diminuir o contador.

--- ex_03_contador/lib/main.dart
+++ ex_03_contador/lib/main.dart
@@ -27,6 +27,24 @@ class CounterPage extends StatefulWidget {
 class _CounterPageState extends State<CounterPage> {
   int counter = 0;
 
+  void incrementCounter() {
+    setState(() {
+      counter++;
+    });
+  }
+  
+    void decrementCounter() {
+    setState(() {
+      counter--;
+    });
+  }
+
+      void resetCounter() {
+    setState(() {
+      counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,17 +58,26 @@ class _CounterPageState extends State<CounterPage> {
               style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-              child: const Text('Aumentar'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: decrementCounter,
+                  child: const Text('Diminuir'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(onPressed: incrementCounter,
+                child: const Text('Aumentar'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(onPressed: resetCounter,
+                child: const Text('Resetar'),
+                ),
+              ],
             ),
           ],
         ),
-      ),
+      )
     );
   }
 }
