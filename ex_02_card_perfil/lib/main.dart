4b4babbb47191d612a1793d32a30958138e75863
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-06 14:28:46 -0300

    Finalização do exercício 02 - Card Perfil.

--- ex_02_card_perfil/lib/main.dart
+++ ex_02_card_perfil/lib/main.dart
@@ -11,7 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: ProfilePage(),
+      );
+  }
+}
+
+class ProfilePage extends StatelessWidget{
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         backgroundColor: Colors.white70,
         appBar: AppBar(
           title: const Text('Card Perfil')),
@@ -32,69 +42,89 @@ class MyApp extends StatelessWidget {
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                CircleAvatar(
-                  radius: 46,
-                  backgroundColor: Colors.blueAccent,
-                  child: Icon(  
-                    Icons.person,
-                    size: 46,
-                    color: Colors.white,
-                    ),
-                  ),
-                SizedBox(height: 16),
-                Text('User Test',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 9),
-                Text('Flutter Developer',
-                  style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                  ),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: const [
-                          Icon(Icons.location_on, color: Colors.blueAccent),
-                          SizedBox(height: 6),
-                          Text('Brasilia'),
-                        ],
-                      ),
-                      Column(
-                        children: const [
-                          Icon(Icons.cake, color: Colors.blueAccent),
-                          SizedBox(height: 6),
-                          Text('27 anos'),
-                        ],
-                      ),
-                      Column(
-                        children: const [
-                          Icon(Icons.email, color: Colors.blueAccent),
-                          SizedBox(height: 6),
-                          Text('usertest@email.com'),
-                        ],
-                      ),
-                        Column(
-                        children: const [
-                          Icon(Icons.code, color: Colors.blueAccent),
-                          SizedBox(height: 6),
-                          Text('Flutter, Dart, Ruby on Rails'),
-                        ],
-                      ),
-                    ],
-                  )
+              children: [
+                buildAvatar(),
+                const SizedBox(height: 15),
+                buildName(),
+                const SizedBox(height: 5),
+                buildInfoRow(),
+                const SizedBox(height: 20),
+                buildActionButton(),
               ],
-                ),
             ),
           ),
         ),
       );
-  }
+
+
+}
+}
+
+Widget buildAvatar() {
+  return const CircleAvatar(
+    radius: 45,
+    backgroundColor: Colors.blueAccent,
+    child: Icon(  
+      Icons.person,
+      size: 45,
+      color: Colors.white,
+      ),
+    );
+}
+
+Widget buildName() {
+  return const Text('User Test',
+    style: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      ),
+    );
+}
+
+
+
+Widget buildInfoRow(){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Column(
+        children: const [
+          Icon(Icons.location_on, color: Colors.blueAccent),
+          SizedBox(height: 6),
+          Text('Brasilia'),
+        ],
+      ),
+      Column(
+        children: const [
+          Icon(Icons.cake, color: Colors.blueAccent),
+          SizedBox(height: 6),
+          Text('27 anos'),
+        ],
+      ),
+      Column(
+        children: const [
+          Icon(Icons.email, color: Colors.blueAccent),
+          SizedBox(height: 6),
+          Text('usertest@email.com'),
+        ],
+      ),
+        Column(
+        children: const [
+          Icon(Icons.code, color: Colors.blueAccent),
+          SizedBox(height: 6),
+          Text('Flutter, Dart, Ruby on Rails'),
+        ],
+      ),
+    ],
+  );
+}
+
+Widget buildActionButton(){
+  return SizedBox(
+    width: double.infinity,
+    child: ElevatedButton(
+      onPressed: () {},
+      child: const Text('Saiba mais'),
+    ),
+  );
 }
