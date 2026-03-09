2948f6f119e39fe74b61aab2ec643f65357e29aa
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-09 11:46:02 -0300

    Adiciona os campos para o usuário digitar P/A.

--- ex_04_imc/lib/main.dart
+++ ex_04_imc/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home:  ImcPage(),
       debugShowCheckedModeBanner: false,
     );
@@ -27,15 +27,70 @@ class ImcPage extends StatefulWidget {
 class _ImcPageState extends State<ImcPage> {
   final TextEditingController _weightController = TextEditingController();
   final TextEditingController _heightController = TextEditingController();
+
+  double? _imc;
+  String aviso = 'Preencha os campos para calcular seu IMC';
+
+  @override
+  void dispose() {
+    _weightController.dispose();
+    _heightController.dispose();
+    super.dispose();
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 199, 197, 197),
       appBar: AppBar(
         title: const Text('Calculadora de IMC'),
       ),
-      body: const Center(
-        child: Text('Aqui IMC'),
+      body: Center(
+        child: Container(
+          width: 340,
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Informe seu peso (kg) e altura (m) para calcular seu IMC',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _weightController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                  labelText: 'Peso (Kg)',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              const SizedBox(height: 16),
+               TextField(
+                controller: _heightController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                  labelText: 'Altura (m)',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
