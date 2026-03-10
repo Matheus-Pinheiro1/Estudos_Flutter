159c9bfc3e5496f8ff5e23a79435b935d60b3ecb
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-10 15:13:53 -0300

    Adiciona a classe Task e a tela de tarefas

--- ex_05_lista_de_tarefas/lib/main.dart
+++ ex_05_lista_de_tarefas/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,56 +11,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Lista de Tarefas',
+      home: Taskpage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class Taskpage extends StatefulWidget {
+  const Taskpage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Taskpage> createState() => _TaskpageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TaskpageState extends State<Taskpage> {
+  final List<Task> tasks = [];
+  final TextEditingController taskController = TextEditingController();
 
-  void _incrementCounter() {
+  void addTask (){
+    final String title = taskController.text.trim();
+
+    if (title.isEmpty) {
+      return;
+    }
     setState(() {
-      _counter++;
+      tasks.add(Task(title: title));
+      taskController.clear();
     });
+
+  }
+
+  @override
+  void dispose() {
+    taskController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+        title: const Text('Lista de Tarefas'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: [ Row(
+            children: [
+              Expanded(child: TextField(
+                controller: taskController,
+                decoration: const InputDecoration(
+                  labelText: 'Nova Tarefa',
+                ),
+              ),
+              ),
+              const SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: addTask,
+                child: const Text('Adicionar'),
+              ),
+            ],
+          )
+          ]
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
 }
+  
