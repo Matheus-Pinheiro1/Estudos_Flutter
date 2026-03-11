c384e578452e729daa8afa8bade716836849e281
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-11 15:50:31 -0300

    Criação e estruturação inicial do projeto de navegação.

---
+++ ex_06_navegacao/lib/models/profile.dart
@@ -0,0 +1,15 @@
+class Profile {
+  final String name;
+  final String role;
+  final String city;
+  final String description;
+
+  Profile({
+   required this.name,
+   required this.role,
+   required this.city,
+   required this.description,
+}
+);
+}
+      
