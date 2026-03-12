5f2c38e7f3a4d053bf1595cb7d6c17bc69cf8549
Matheus Pinheiro <matheushenriquepls2@hotmail.com>
2026-03-12 16:16:04 -0300

    Inicial Commit Ex 07 lista POO

---
+++ ex_07_catalogo_poo/android/app/src/main/kotlin/com/example/ex_07_catalogo_poo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ex_07_catalogo_poo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ex_07_catalogo_poo/lib/models/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  final String name;
+  final double price;
+  final String category;
+  final String description;
+  final bool isOnSale;
+
+  const Product({
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.description,
+    required this.isOnSale,
+  });
+}
